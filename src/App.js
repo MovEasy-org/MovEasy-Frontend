140ceda391943e6f86400584c1bd075e548add37
Yash Sharma <yashsharma1859@gmail.com>
2021-05-31 23:21:08 +0530

    Removed vulnerable libraries from main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
-import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
 	return (
-		<ChakraProvider>
-			<Router>
-				<Routes />
-			</Router>
-		</ChakraProvider>
+		<Router>
+			<Routes />
+		</Router>
 	);
 };
 
