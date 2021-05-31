04cafd4b0074d0abada08c4dd25fe1a62ee96ce0
Yash Sharma <yashsharma1859@gmail.com>
2021-05-31 23:24:41 +0530

    Updated Hero Section

--- src/modules/LandingPage/_components/_HeroSection/Hero.js
+++ src/modules/LandingPage/_components/_HeroSection/Hero.js
@@ -3,26 +3,26 @@ import Button from "../../../../components/Button/Button";
 import "./Hero.scss";
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      <div className="hero-hp">
-        <h1 className="Hero-heading">
-          <text className="cyan">Mov</text>Easy
-        </h1>
-        <p className="hero-para">The Packers and Movers Company</p>
-        <div className="hero-btn">
-          <Button
-            type="submit"
-            ButtonStyle="btn-primary"
-            ButtonSize="btn-medium"
-            onClick={undefined}
-          >
-            Explore
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="hero-container">
+			<div className="hero-hp">
+				<h1 className="Hero-heading">
+					<text className="cyan">Mov</text>Easy
+				</h1>
+				<p className="hero-para">The Packers and Movers Company</p>
+				<div className="hero-btn">
+					<Button
+						type="submit"
+						ButtonStyle="btn-primary"
+						ButtonSize="btn-medium"
+						onClick={undefined}
+					>
+						Get Started
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
