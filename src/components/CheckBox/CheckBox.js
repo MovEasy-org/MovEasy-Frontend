70039b5738ae42d276c3d2c0e27d6a8c49f44892
Mihir Verma <mihirv7781@gmail.com>
2021-05-31 14:03:31 +0530

    [mv][story]-check box updtaed with styles and props

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -2,28 +2,32 @@ import React, { useState } from "react";
 import "./CheckBox.scss";
 
 
-const CheckBox = ({ label }) => {
+const CheckBox = ({ label,handleChnage, arrayState, setArrayState }) => {
 
   const [checked, setChecked] =useState(true);
-
   const valueset = () => {
-    if (!checked) {
-      return label;
+    if (checked) {
+      return label
     } else {
-      return "";
+      return ""
     }
   };
 
   console.log(checked);
+  let classes= checked ? 'active-check':'inactive-check';
   return (
     <>
-      <div class="hiddenCB">
-        <label>
+      <div className={classes}>
+        <label className="checkbox-label">
           <input
+            name={label}
             type="checkbox"
             defaultChecked={checked}
             value={valueset()}
             onChange={(e) =>setChecked(!checked)}
+            handleChnage={handleChnage}
+            arrayState={arrayState}
+            setArrayState={setArrayState}
           />
           label
         </label>
