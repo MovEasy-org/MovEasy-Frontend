4de38c52549972ee70f3279406e530ae2bfb775f
Yash Sharma <yashsharma1859@gmail.com>
2021-05-31 18:28:04 +0530

    Fixed label of Checkbox

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -2,52 +2,52 @@ import React, { useState } from "react";
 import "./CheckBox.scss";
 
 const CheckBox = ({
-  label,
-  handleChnage,
-  arrayState,
-  setArrayState,
-  category,
+	label,
+	handleChnage,
+	arrayState,
+	setArrayState,
+	category,
 }) => {
-  const [checked, setChecked] = useState(true);
-  const valueset = () => {
-    if (checked) {
-      return label;
-    } else {
-      return "";
-    }
-  };
+	const [checked, setChecked] = useState(true);
+	const valueset = () => {
+		if (checked) {
+			return label;
+		} else {
+			return "";
+		}
+	};
 
-  console.log(checked);
+	console.log(checked);
 
-  return (
-    <>
-      <div
-        className={
-          category === "user"
-            ? (checked
-              ? "active-check"
-              : "inactive-check")
-            : (checked
-            ? "trans-active-check"
-            : "trans-inactive-check")
-        }
-      >
-        <label className="checkbox-label">
-          <input
-            name={label}
-            type="checkbox"
-            defaultChecked={checked}
-            value={valueset()}
-            onChange={(e) => setChecked(!checked)}
-            handleChnage={handleChnage}
-            arrayState={arrayState}
-            setArrayState={setArrayState}
-          />
-          label
-        </label>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={
+					category === "user"
+						? checked
+							? "active-check"
+							: "inactive-check"
+						: checked
+						? "trans-active-check"
+						: "trans-inactive-check"
+				}
+			>
+				<label className="checkbox-label">
+					<input
+						name={label}
+						type="checkbox"
+						defaultChecked={checked}
+						value={valueset()}
+						onChange={(e) => setChecked(!checked)}
+						handleChnage={handleChnage}
+						arrayState={arrayState}
+						setArrayState={setArrayState}
+					/>
+					{label}
+				</label>
+			</div>
+		</>
+	);
 };
 
 export default CheckBox;
--- src/modules/LandingPage/LandingPage.js
+++ src/modules/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ import About from "../../modules/LandingPage/_components/_AboutSection/About";
 import FeatureCards from "./_components/_FeatureCard/FeatureWrapper";
 import WhyWrapper from "./_components/_WhyMovEasy/WhyWrapper";
 import Testimonials from "./_components/_TestimonialCard/_TestimonialCard";
+
 const LandingPage = () => {
 	return (
 		<div className="landing-page">
