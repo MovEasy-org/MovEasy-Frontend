83e2e92dee208df354b740233aa45e839d5419b2
Yash Sharma <yashsharma1859@gmail.com>
2021-06-06 04:39:01 +0530

    fixed the history card component

--- src/modules/Customer/History/History.js
+++ src/modules/Customer/History/History.js
@@ -1,26 +1,70 @@
 import React from "react";
 import "./History.scss";
 import HistoryCard from "./_components/HistoryCard/HistoryCard";
-const History = ({ customerHistory }) => {
-  return (
-    <>
-      <div className="history-container">
-      <h1 className="history-head">History</h1>
-        <div className="history-map-container">
-          {customerHistory.map((history, index) => {
-            <HistoryCard
-            key={index}
-              status={history.status}
-              from={history.from}
-              to={history.to}
-              date={history.date}
-              time={history.time}
-            />;
-          })}
-        </div>
-      </div>
-    </>
-  );
+const History = () => {
+	const customerHistory = [
+		{
+			status: true,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+		{
+			status: false,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+		{
+			status: false,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+		{
+			status: true,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+		{
+			status: false,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+		{
+			status: false,
+			from: "384 scheme71 near c21 mall, Indore-452008",
+			to: "384 scheme71 near c21 mall, Indore-452008",
+			date: "12/01/2021",
+			time: "12:00 PM",
+		},
+	];
+	return (
+		<>
+			<div className="history-container wrapper">
+				<h2 className="history-head">History</h2>
+				<div className="history-map-container">
+					{customerHistory.map((history, index) => (
+						<HistoryCard
+							key={index}
+							status={history.status}
+							from={history.from}
+							to={history.to}
+							date={history.date}
+							time={history.time}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default History;
