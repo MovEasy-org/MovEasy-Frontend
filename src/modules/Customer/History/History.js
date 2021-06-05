dfa8ebb7701b7427710338f1387c859c4226bdd1
Mihir Verma <mihirv7781@gmail.com>
2021-06-06 02:02:52 +0530

    history page added

--- src/modules/Customer/History/History.js
+++ src/modules/Customer/History/History.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./History.scss";
-const History = () => {
-    return (
-        <div>
-            history
+import HistoryCard from "./_components/HistoryCard/HistoryCard";
+const History = ({ customerHistory }) => {
+  return (
+    <>
+      <div className="history-container">
+      <h1 className="history-head">History</h1>
+        <div className="history-map-container">
+          {customerHistory.map((history, index) => {
+            <HistoryCard
+            key={index}
+              status={history.status}
+              from={history.from}
+              to={history.to}
+              date={history.date}
+              time={history.time}
+            />;
+          })}
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default History
+export default History;
