22939acffaa317a8613c64383f7cb525f5e4fee6
Yash Sharma <yashsharma1859@gmail.com>
2021-06-06 14:20:59 +0530

    added history

--- src/modules/Customer/History/History.js
+++ src/modules/Customer/History/History.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./History.scss";
 import HistoryCard from "./_components/HistoryCard/HistoryCard";
 const History = ({ customerHistory }) => {
-  return (
-    <>
-      <div className="history-container">
-      <h1 className="history-head">History</h1>
-        <div className="history-map-container">
-          {customerHistory.map((history, index) => {
-            <HistoryCard
-            key={index}
-              status={history.status}
-              from={history.from}
-              to={history.to}
-              date={history.date}
-              time={history.time}
-            />;
-          })}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="history-container">
+				<h1 className="history-head">History</h1>
+				<div className="history-map-container">
+					{customerHistory.map((history, index) => (
+						<HistoryCard
+							key={index}
+							status={history.status}
+							from={history.from}
+							to={history.to}
+							date={history.date}
+							time={history.time}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default History;
